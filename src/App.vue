cf54fdf39e538b53105409a0cd1675d8276625e9
Mauricio <mauricionunesdasilvanunes@gmail.com>
2022-06-10 19:58:50 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="login">
+    <login />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import login from './components/login.vue'
 
 export default {
-  name: 'App',
+  name: 'LayoutDefault',
+
   components: {
-    HelloWorld
+    login
+  },
+
+  setup() {
+    return {
+      leftDrawerOpen: ref(false)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body, html {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #5533ff;
 }
 </style>
